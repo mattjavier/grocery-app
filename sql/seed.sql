8b305ebceb932f99523216a323bdc8c4f8a330f3
Matt Javier <matthewrjavier@gmail.com>
2020-08-25 13:40:30 -0700

    changes

---
+++ sql/seed.sql
@@ -0,0 +1,14 @@
+USE grocery_db;
+
+INSERT INTO groceries (name, quantity, cost)
+VALUES 
+('milk', 50, 5.00),
+('juice', 40, 3.50),
+('chips', 60, 3.00),
+('apples', 20, 1.50),
+('oreos', 40, 6.00),
+('beer', 20, 8.00),
+('eggs', 20, 10.00),
+('coffee', 50, 12.00),
+('bacon', 10, 7.00),
+('cheese', 10, 8.00);
