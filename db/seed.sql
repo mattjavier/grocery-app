a485914a033b9da42bed19473c329b349684656d
Matt Javier <matthewrjavier@gmail.com>
2020-08-26 10:25:50 -0700

    updated sql files

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,14 @@
 USE grocery_db;
 
-INSERT INTO groceries (name, quantity, cost)
+INSERT INTO groceries (name, quantity, cost, purchased)
 VALUES 
-('milk', 50, 5.00),
-('juice', 40, 3.50),
-('chips', 60, 3.00),
-('apples', 20, 1.50),
-('oreos', 40, 6.00),
-('beer', 20, 8.00),
-('eggs', 20, 10.00),
-('coffee', 50, 12.00),
-('bacon', 10, 7.00),
-('cheese', 10, 8.00);
+('milk', 50, 5.00, false),
+('juice', 40, 3.50, false),
+('chips', 60, 3.00, true),
+('apples', 20, 1.50, true),
+('oreos', 40, 6.00, false),
+('beer', 20, 8.00, false),
+('eggs', 20, 10.00, true),
+('coffee', 50, 12.00, true),
+('bacon', 10, 7.00, false),
+('cheese', 10, 8.00, false);
